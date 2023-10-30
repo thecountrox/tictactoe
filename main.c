a5d83ee86b7f84154ea7060248b65c608b2f9099
thecountrox <74064922+thecountrox@users.noreply.github.com>
2023-10-30 21:40:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 int checkwin();
 int main()
     {
